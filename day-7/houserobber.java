6c094a0ade8aa9f4a3c850db3781a74b731f43d6
kratika agrawal <misskratikaagrawal@gmail.com>
2025-10-26 13:25:02 +0530

    ass-7

---
+++ day-7/houserobber.java
@@ -0,0 +1,22 @@
+public class houserobber{
+    public static void main(String[] args) {
+        int[] houses = {2, 7, 9, 3, 1};
+        System.out.println("Maximum amount that can be robbed: " + rob(houses));
+    }
+    public static int rob(int[] nums){
+        
+        if (nums == null || nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+
+        return dp[n - 1];
+    }
+}
---
+++ day-7/max_sum.java
@@ -0,0 +1,22 @@
+public class max_sum {
+    public static void main(String[] args) {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.print(Maximum_Sum(nums));
+    }
+     public static int Maximum_Sum(int[] arr) {
+		int ans=Integer.MIN_VALUE;
+		int sum=0;
+		for(int i=0;i<arr.length;i++) {
+			sum+=arr[i];
+			ans=Math.max(ans, sum);
+			if(sum<0) {
+				sum=0;
+			}
+		}
+		return ans;
+	}
+
+}
+   
+        
+
